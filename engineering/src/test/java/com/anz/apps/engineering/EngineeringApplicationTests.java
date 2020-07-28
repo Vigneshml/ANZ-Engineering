1bf0410cd62227602019a0267626a8d607ac0790
Vigneshml <39254968+Vigneshml@users.noreply.github.com>
2020-07-28 13:55:45 +0530

    Add files via upload

--- engineering/src/test/java/com/anz/apps/engineering/EngineeringApplicationTests.java
+++ engineering/src/test/java/com/anz/apps/engineering/EngineeringApplicationTests.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 import java.util.Date;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,8 +15,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.anz.apps.engineering.model.AccountTransaction;
 import com.anz.apps.engineering.model.UserAccount;
+import com.anz.apps.engineering.repository.AccountTransactionRepository;
 import com.anz.apps.engineering.repository.UserAccountRepository;
+import com.anz.apps.engineering.service.AccountTransactionService;
 import com.anz.apps.engineering.service.UserAccountService;
 
 @RunWith(SpringRunner.class)
@@ -28,13 +32,80 @@ public class EngineeringApplicationTests {
 	@MockBean
 	UserAccountRepository userAccountRepository;
 
-	@Test
-	public void test_getAllUserAndTheirAccDetails_greenPath() {
+	@Autowired
+	AccountTransactionService accountTransactionService;
 
+	@MockBean
+	AccountTransactionRepository accountTransactionRepository;
+
+	@Test
+	public void test_getAllUserAndTheirAccDetails_greenPath_singleRecordScenario() {
+		when(userAccountRepository.findAll())
+				.thenReturn(Stream.of(new UserAccount(12, "Vignesh", "ACCNUM123", "SA", "Savings", null, "USD", 84755))
+						.collect(Collectors.toList()));
+		List<UserAccount> userAccountDetails = userAccountService.getAllUserAndTheirAccDetails();
+		assertEquals(1, userAccountDetails.size());
+		assertEquals("Vignesh", userAccountDetails.get(0).getUserName());
+		assertEquals("SA", userAccountDetails.get(0).getAccName());
+	}
+
+	@Test
+	public void test_getAllUserAndTheirAccDetails_greenPath_multiRecordScenario() {
 		when(userAccountRepository.findAll()).thenReturn(Stream
-				.of(new UserAccount(111, "ACCNUM123", "Vignesh", "SA", "Savings", new Date("2020-02-05"), "USD", 84755))
+				.of(new UserAccount(12, "Vignesh", "ACCNUM123", "SA", "Savings", new Date(2020, 05, 06), "USD", 90000),
+						new UserAccount(13, "Victor", "ACCNUM123", "Current Account", "Savings", new Date(2020, 05, 06),
+								"USD", 80000))
 				.collect(Collectors.toList()));
-		assertEquals(1, userAccountService.getAllUserAndTheirAccDetails().size());
+		List<UserAccount> userAccountDetails = userAccountService.getAllUserAndTheirAccDetails();
+		assertEquals(2, userAccountDetails.size());
+		assertEquals("Vignesh", userAccountDetails.get(0).getUserName());
+		assertEquals("SA", userAccountDetails.get(0).getAccName());
+		assertEquals(90000, userAccountDetails.get(0).getBalance(), 0);
+		assertEquals("Victor", userAccountDetails.get(1).getUserName());
+		assertEquals("Current Account", userAccountDetails.get(1).getAccName());
+		assertEquals(80000, userAccountDetails.get(1).getBalance(), 0);
+	}
+
+	@Test
+	public void test_getAccDetailsByUserName_greenPath_singleRecordsScenario() {
+		when(userAccountRepository.findByUserName("Vignesh"))
+				.thenReturn(Stream.of(new UserAccount(12, "Vignesh", "ACCNUM123", "SA", "Savings", null, "USD", 84755))
+						.collect(Collectors.toList()));
+		List<UserAccount> userAccountDetailsByUserName = userAccountService.getAccDetailsByUserName("Vignesh");
+		assertEquals(1, userAccountDetailsByUserName.size());
+		assertEquals("Vignesh", userAccountDetailsByUserName.get(0).getUserName());
+		assertEquals("SA", userAccountDetailsByUserName.get(0).getAccName());
+	}
+
+	@Test
+	public void test_getAccDetailsByUserName_greenPath_multipleRecordsScenario() {
+		when(userAccountRepository.findByUserName("Vignesh")).thenReturn(Stream
+				.of(new UserAccount(12, "Vignesh", "ACCNUM123", "SA", "Savings", new Date(2020, 05, 06), "USD", 90000),
+						new UserAccount(13, "Victor", "ACCNUM123", "Current Account", "Savings", new Date(2020, 05, 06),
+								"USD", 80000))
+				.collect(Collectors.toList()));
+		List<UserAccount> userAccountDetailsByUserName = userAccountService.getAccDetailsByUserName("Vignesh");
+		assertEquals(2, userAccountDetailsByUserName.size());
+		assertEquals("Vignesh", userAccountDetailsByUserName.get(0).getUserName());
+		assertEquals("SA", userAccountDetailsByUserName.get(0).getAccName());
+		assertEquals(90000, userAccountDetailsByUserName.get(0).getBalance(), 0);
+		assertEquals("Victor", userAccountDetailsByUserName.get(1).getUserName());
+		assertEquals("Current Account", userAccountDetailsByUserName.get(1).getAccName());
+		assertEquals(80000, userAccountDetailsByUserName.get(1).getBalance(), 0);
+	}
+
+	@Test
+	public void test_getAllTransactions_greenPath_singleRecord() {
+		when(accountTransactionRepository.findAll()).thenReturn(Stream.of(
+				new AccountTransaction(111, "ACC1234", "SA123", new Date(2020, 05, 06), "INR", 80000.00, "Savings", ""))
+				.collect(Collectors.toList()));
+		List<AccountTransaction> allTransactions = accountTransactionService.getAllTransactions();
+		assertEquals(1, allTransactions.size());
+		assertEquals("SA123", allTransactions.get(0).getAccName());
+		assertEquals("ACC1234", allTransactions.get(0).getAccNumber());
+		assertEquals("INR", allTransactions.get(0).getCurrency());
+		assertEquals("Savings", allTransactions.get(0).getTransactionType());
+
 	}
 
 }
